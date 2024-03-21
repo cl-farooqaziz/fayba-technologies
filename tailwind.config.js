11fb43977344f8f1077fc0170f929a0dbf561115
cl-farooqaziz <dev.faz@productions-dev.com>
2024-03-22 02:47:44 +0500

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+/** @type {import("tailwindcss").Config} */
+export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      container: {
+        center: true,
+        padding: '3rem',
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1200px',
+          xl: '1360px',
+          '2xl': '1500px',
+        }
       },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1300px',
+        '2xl': '1500px',
+        'xxl': '1700px',
+        '3xl': '1900px',
+      },
+      fontFamily: {
+        sans: ["var(--font-primary)"],
+        poppins: ["var"]
+      },
+      colors: {
+        "primary": { 100: "#246475" },
+        "secondary": { 100: "#27112c" },
+      }
     },
   },
   plugins: [],
-};
+}
